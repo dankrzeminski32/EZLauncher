294843411bcd751b14372a573aa7b823e3af258b
dankrzeminski32 <dankrzeminski32@gmail.com>
2023-01-26 18:13:01 -0800

    update structure

---
+++ src/games/steam.py
@@ -0,0 +1,5 @@
+# Use the command below to run a Steam game
+# subprocess.call(r"C:\Program Files (x86)\Steam\Steam.exe -applaunch 578080")
+# 578080 represents the steam ID
+# C:\Program Files (x86)\Steam\steamapps\appmanifest_id -> is where you find the steam games and all their id's/information
+# We'll need to parse those files for the ids and then use them in the subprocess command
