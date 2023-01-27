ea7822b4c1acba5b8784ded5f79b3f40f52fa106
Ethan <ethankvachkoff@gmail.com>
2023-01-26 20:14:42 -0600

    Changed tkinter to customtkinter

--- gui/main.py
+++ gui/main.py
@@ -1,8 +1,13 @@
-import tkinter as tk
+import customtkinter
 
+customtkinter.set_appearance_mode("dark")
+customtkinter.set_default_color_theme("dark-blue")
 
-def base():
-    r = tk.Tk()
-    r.title("EZLauncher")
-    r.geometry("900x750")
-    r.mainloop()
+root = customtkinter.CTk()
+root.geometry("500x350")
+root.title("EZLauncher")
+
+frame = customtkinter.CTkFrame(master=root)
+frame.pack(pady=20, padx=60, fill="both", expand=True)
+
+root.mainloop()
