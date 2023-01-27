1b8f7092759dcc4e06d7ef2f5fefb906f4013fcd
Ethan <ethankvachkoff@gmail.com>
2023-01-26 21:32:18 -0600

    Added more buttons to the grid

--- src/gui/main.py
+++ src/gui/main.py
@@ -13,7 +13,7 @@ class App(customtkinter.CTk):
 
         self.navigation_frame = customtkinter.CTkFrame(self, corner_radius=0)
         self.navigation_frame.grid(row=0, column=0, sticky="nsew")
-        self.navigation_frame.grid_rowconfigure(4, weight=1)
+        self.navigation_frame.grid_rowconfigure(6, weight=1)
 
         self.navigation_frame_label = customtkinter.CTkLabel(
             self.navigation_frame,
@@ -38,6 +38,58 @@ class App(customtkinter.CTk):
         )
 
         self.home_button.grid(row=1, column=0, sticky="ew")
+
+        self.steam_button = customtkinter.CTkButton(
+            self.navigation_frame,
+            corner_radius=0,
+            height=40,
+            border_spacing=10,
+            text="Steam",
+            fg_color="transparent",
+            text_color=("gray10", "gray90"),
+            hover_color=("gray70", "gray30"),
+            anchor="w",
+        )
+        self.steam_button.grid(row=2, column=0, sticky="ew")
+
+        self.battle_net_button = customtkinter.CTkButton(
+            self.navigation_frame,
+            corner_radius=0,
+            height=40,
+            border_spacing=10,
+            text="Battle.net",
+            fg_color="transparent",
+            text_color=("gray10", "gray90"),
+            hover_color=("gray70", "gray30"),
+            anchor="w",
+        )
+        self.battle_net_button.grid(row=3, column=0, sticky="ew")
+
+        self.epic_games_button = customtkinter.CTkButton(
+            self.navigation_frame,
+            corner_radius=0,
+            height=40,
+            border_spacing=10,
+            text="Epic Games",
+            fg_color="transparent",
+            text_color=("gray10", "gray90"),
+            hover_color=("gray70", "gray30"),
+            anchor="w",
+        )
+        self.epic_games_button.grid(row=4, column=0, sticky="ew")
+
+        self.ubisoft_button = customtkinter.CTkButton(
+            self.navigation_frame,
+            corner_radius=0,
+            height=40,
+            border_spacing=10,
+            text="Ubisoft",
+            fg_color="transparent",
+            text_color=("gray10", "gray90"),
+            hover_color=("gray70", "gray30"),
+            anchor="w",
+        )
+        self.ubisoft_button.grid(row=4, column=0, sticky="ew")
 
     def button_callback(self):
         print("button pressed")
